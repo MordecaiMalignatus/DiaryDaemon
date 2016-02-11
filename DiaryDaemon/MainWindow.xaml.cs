281d5ac4b9efca9eb658dbdfc117fbcb81d0d7a6
Az4reus <az4reus@gmail.com>
2016-02-11 06:20:56 +0100

    Initial commit

---
+++ DiaryDaemon/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiaryDaemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            input.Focus();
+        }
+
+        private void _onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                Log(input.Text);
+                input.Clear();
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void Log(string content)
+        {
+            var fileName = FindFileName();
+            using (var sw = new StreamWriter(fileName))
+            {
+                sw.WriteLine(content);
+            }
+        }
+
+        private string FindFileName()
+        {
+            return "goddamnit VS";
+        }
+    }
+}
